4f25a77357434cecfdb522bba9bf90a1425b77e0
manuel <manuelflorentino768@gmail.com>
2023-08-18 12:31:01 -0600

    confirmado

---
+++ main.js
@@ -0,0 +1,13 @@
+var body = document.body;
+//alerta de web en desarrollo 
+function load() {
+  //alert("Esta web aun esta en fase BETA (desarrollo) por lo que sus funciones aun no estan completas. not responsive")
+}
+//da atras a un elemento
+function menos() {
+  window.history.back();
+}
+//opcion desactivada
+function disabled() {
+  //alert("Está opción aun esta en desarrollo, no disponible por el momento");
+}
